1e75ba01ee1e1fa9f9612d846f188adfeaff795d
xxxuduo <32809896+xxxuduo@users.noreply.github.com>
2019-03-26 02:02:40 -0700

    Update index

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import "./styles/styles.css";
 
 import Cart from "./Cart";
 
-
 // Add the Cart component to the `#root` element below!
 // YOUR CODE HERE
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Cart />, rootElement);
