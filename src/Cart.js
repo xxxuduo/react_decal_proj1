9a67acc05078144b23bf79deb5f2e41ab0076f7d
xxxuduo <32809896+xxxuduo@users.noreply.github.com>
2019-03-26 01:52:05 -0700

    Update Cart

--- src/Cart.js
+++ src/Cart.js
@@ -1,16 +1,28 @@
 import React from "react";
 import "./styles/cart.css";
+import Product from "./Product";
+import ProductsData from "./Data";
 
 class Cart extends React.Component {
-
+  getProducts() {
+    const products = ProductsData.products.map(product => (
+      <Product
+        productName={product.name}
+        price={product.cost}
+        limit={product.stock}
+      />
+    ));
+    return products;
+  }
   render() {
+    const products = this.getProducts();
     return (
-        <div className="page-content">
-            <h2>Add your products here!</h2>
-        </div>
+      <div>
+        <h2>Add your products here!</h2>
+        <div className="page-content">{products}</div>
+      </div>
     );
   }
-
 }
 
 export default Cart;
