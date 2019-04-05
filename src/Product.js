7945aa69b257bd6fb8a7a696e5b1778c4144a33d
xxxuduo <32809896+xxxuduo@users.noreply.github.com>
2019-04-04 21:08:23 -0700

    Finish Product.js

--- src/Product.js
+++ src/Product.js
@@ -1,11 +1,32 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+import "./styles/cart.css";
+import "./Data.js";
+class Product extends React.Component {
+  render() {
+    return (
+      <div class="cards">
+        <div class="content">
+          <div class="name">{this.props.productName}</div>
+          <div class="price">Price: ${this.props.price}</div>
+        </div>
+        <div class="ui bottom attached button" onClick={() => this.props.onAddToCart(this.props.productName, this.props.price)}>
+          <i class="add icon" />
+          Add Cart
+        </div>
+        <div class="ui bottom attached button" onClick={() => this.props.onRemoveFromCart(this.props.productName, this.props.price)}>
+          <i className="remove icon" />
+          Remove from Cart
+        </div>
+      </div>
+    );
+  }
+}
+export default Product;
 
-import "./styles/styles.css";
-
-import Cart from "./Cart";
-
-// Add the Cart component to the `#root` element below!
-// YOUR CODE HERE
-const rootElement = document.getElementById("root");
-ReactDOM.render(<Cart />, rootElement);
+Product.propTypes = {
+  productName: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  onAddToCart: PropTypes.func.isRequired,
+  onRemoveFromCart: PropTypes.func.isRequired
+};
